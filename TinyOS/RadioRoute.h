60d401f09c4ff374956e16ddaaa58cb976872470
Roberto99-ops <roberto.molgora@mail.polimi.it>
2023-06-11 12:12:42 +0200

    first commit

---
+++ TinyOS/RadioRoute.h
@@ -0,0 +1,18 @@
+
+
+#ifndef RADIO_ROUTE_H
+#define RADIO_ROUTE_H
+
+typedef nx_struct radio_route_msg {
+	nx_uint8_t type; //1 if data message and 2 if ack message
+	nx_uint16_t sender; //sender node
+	nx_uint16_t gateway; //sender gateway
+	nx_uint16_t destination; //used just by gateways and server
+	nx_uint8_t value; //payload
+} radio_route_msg_t;
+
+enum {
+  AM_RADIO_COUNT_MSG = 10,
+};
+
+#endif
